63dc50f74d3b3b60d42078e849b58118e92584a6
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-25 15:45:52 +0000

    - Add requirements file. - Attempt authentication.

--- app/views.py
+++ app/views.py
@@ -1,13 +1,25 @@
+from django.conf import settings
 from django.shortcuts import render
 
 from .forms import AuthForm
 
+from requestor import requestor
+
 def login(request):
     if request.method == 'POST':
-        pass
-    else:
-        form = AuthForm()
+        username = request.POST['username']
+        password = request.POST['password']
 
+        data = {
+            'username': username,
+            'password': password,
+        }
+
+        response = requestor(settings.AUTH_URL, data=data)
+    else:
+        pass
+
+    form = AuthForm()
     context = {'form': form}
     return render(request, 'login.html', context)
 
