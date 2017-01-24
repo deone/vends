2ba315a288578331c94601e4e16eebdc48fcccb9
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-24 23:57:27 +0000

    Render template.

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def index(request):
-    pass
+def index(request, template='vend_standard.html'):
+    return render(request, template)
