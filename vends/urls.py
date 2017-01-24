0cbdc710f54e101c4883dedd50925bc54dfc843c
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-01-24 23:36:18 +0000

    Add Django logout view.

--- vends/urls.py
+++ vends/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from app.forms import AuthForm
 
 urlpatterns = [
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/login/$', auth_views.login, {
         'template_name': 'login.html',
         'authentication_form': AuthForm
